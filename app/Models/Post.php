311128e0c1b0e6382a5c64e1026ca4044e857f6a
tomoki <tmdesign.0223@gmail.com>
2024-03-25 22:29:03 +0900

    全行の取得と単一の行の取得追加

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,8 +4,23 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class Post extends Model
 {
     use HasFactory;
+
+    //全行の取得
+    public static function index()
+    {
+        $posts = DB::table('posts')->get();
+        return $posts;
+    }
+
+    //単一の行/カラムを取得
+    public function getSingle()
+    {
+        $post = DB::table('posts')->where('created_at', '2024-03-17 06:17:42')->first();
+        return $post;
+    }
 }
